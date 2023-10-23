d5568046a9ee5275042949269695e50b9a56dd66
21120502 <37113714+Flashback054@users.noreply.github.com>
2023-10-23 17:36:51 +0700

    Add local guard for auth

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
+import { LocalAuthGuard } from './guards/local.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalAuthGuard],
 })
 export class AuthModule {}
